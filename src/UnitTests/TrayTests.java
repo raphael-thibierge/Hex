a9120ae2a8d8c6cd5def9e80168be45e6ccede5a
E3GE <arthur.pava@outlook.com>
2015-11-21 10:41:23 +0100

    update test_victory

--- src/UnitTests/TrayTests.java
+++ src/UnitTests/TrayTests.java
@@ -16,7 +16,6 @@ public class TrayTests {
 		Tray t = new Tray(5,5);
 		TrayCoords tc = new TrayCoords(1,3);
 		
-		
 		assertTrue("Mustn't be null", t.getCell(tc) != null);
 		assertEquals("color must be EMPTY", t.getCell(tc).getColor(), Color.EMPTY);
 		assertEquals("coord must be equals", t.getCell(tc).getCoords(),tc);
@@ -51,20 +50,6 @@ public class TrayTests {
 	@Test
 	public void tray_testVictory()
 	{
-		
-		// BLEU : de gauche à droite
-		// ROUGE : de haut en bas
-		
-		// Teste pour chaque joueur :
-		
-		// Une grille sans gagnant
-		// Une grille où rouge gagne
-		// Une grille où bleu gagne
-		// Une grille où bleu & rouge gagne (thoriquement impossible)
-		// Une grille où bleu à le placement gagnant de rouge
-		// Une grille où rouge à le placement gagnant de bleu
-		
-		
 		// Pas de gagnant
 		
 		Tray t = new Tray(3,3);
@@ -73,59 +58,23 @@ public class TrayTests {
 		
 		//Bleu gagne, rouge perd
 		
-		t.putTocken(new TrayCoords(0,0), Color.BLUE);
-		t.putTocken(new TrayCoords(1,1), Color.BLUE);
-		t.putTocken(new TrayCoords(2,2), Color.BLUE);
-		   
-		//B00
-		//0B0
-		//00B
-		   
+		t.putTocken(new TrayCoords(0,1), Color.BLUE);
+		t.putTocken(new TrayCoords(1,0), Color.BLUE);
+		t.putTocken(new TrayCoords(2,0), Color.BLUE);
+		      
 		assertTrue("blue won",t.testVictory(Color.BLUE));
 		assertFalse("red didn't won",t.testVictory(Color.RED));
-		 
-		//Bleu gagne, rouge gagne
-		
-		t.putTocken(new TrayCoords(0,2), Color.RED);
-		t.putTocken(new TrayCoords(2,1), Color.RED);
-		t.putTocken(new TrayCoords(1,0), Color.RED);
-		
-		//B0R
-		//RBR
-		//0RB
-		
-		assertTrue("blue won",t.testVictory(Color.BLUE));
-		assertTrue("red won",t.testVictory(Color.RED));
 		
 		// Rouge gagne, bleu perd
 		
-		t.getCell(new TrayCoords(2,2)).setColor(Color.EMPTY);
+		t.getCell(new TrayCoords(2,0)).setColor(Color.EMPTY);
 		
-		//B0R
-		//RBR
-		//0R0
+		t.putTocken(new TrayCoords(0,2), Color.RED);
+		t.putTocken(new TrayCoords(1,1), Color.RED);
+		t.putTocken(new TrayCoords(2,0), Color.RED);
 		
 		assertTrue("red won",t.testVictory(Color.RED));
 		assertFalse("blue didn't won",t.testVictory(Color.BLUE));
-		
-		// Grille où bleu à le placement gagnant de rouge et vis versa
-		
-		Tray t2 = new Tray(3,3);
-		
-		t2.putTocken(new TrayCoords(0,0), Color.BLUE);
-		t2.putTocken(new TrayCoords(1,1), Color.BLUE);
-		t2.putTocken(new TrayCoords(2,0), Color.BLUE);
-		
-		t2.putTocken(new TrayCoords(1,0), Color.RED);
-		t2.putTocken(new TrayCoords(0,1), Color.RED);
-		t2.putTocken(new TrayCoords(1,2), Color.RED);
-		
-		//BR0
-		//RBR
-		//B00
-		
-		assertFalse("blue didn't won",t2.testVictory(Color.BLUE));
-		assertFalse("red didn't won",t2.testVictory(Color.RED));
 	}
 	
 	@Test
